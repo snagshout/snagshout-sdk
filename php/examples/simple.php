d9582ac6ee58c96588eaecf7e670edb3284e3ba6
Eduardo Trujillo <ed@chromabits.com>
2016-10-24 19:04:26 -0700

    chore(php): Remove dummy keys

--- php/examples/simple.php
+++ php/examples/simple.php
@@ -7,8 +7,8 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use SellerLabs\Snagshout\Client;
 
 $client = new Client(
-    "b6824d25898b93387146c6d8842a9234c371b0c11471891bb042dc882e2dfe12264cd7b699db35272a7033184b440d3534234e673613bf13b35d1ebfee69a12c",
-    "121ad4b2459d7c5aaefcac7c090abc9ff44acc1ab7c1d936e7a699d576f3a8abac104307540d38ebe814d66176d40b46e32327b65cc8e8e34ef640e219e0e81e"
+    "PUBLIC_ID",
+    "PRIVATE_KEY"
 );
 
 var_export(
