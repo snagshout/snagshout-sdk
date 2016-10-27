d7efe559962a4f4dcf030d58fe97c260da30c099
Eduardo Trujillo <ed@chromabits.com>
2016-10-26 18:43:48 -0700

    feat(js): Update example to use latest spec

--- js/examples/simple.js
+++ js/examples/simple.js
@@ -2,20 +2,34 @@
 
 var SnagshoutClient = require('../src/SnagshoutClient');
 
+// We begin by constructing an instance of the client. This will pull the
+// latest Swagger spec from GitHub, and dynamically build a client. It returns
+// a promise that when resolved provides an instance to the generated client.
 SnagshoutClient(
     'PUBLIC_ID',
     'PRIVATE_KEY'
 )
-  .then(function (client) {
-    return client.default.get_api_v1_campaigns({}, {});
-  })
-  .then(function (response) {
-    console.log('Cool deals from the web:');
+.then(function (client) {
+  // All API methods can be called by first using the resource name and then
+  // the operation name.
+  //
+  // Examples:
+  //
+  // - client.campaign.getCampaigns
+  // - client.category.getCategories
+  // - client.front.getStatus
+  //
+  return client.campaign.getCampaigns({}, {});
+})
+.then(function (response) {
+  // Below we print out a subset of all the information returned by the
+  // response. To see everything returned, try console.log(response).
+  console.log('Cool deals from the web:');
 
-    response.obj.data.forEach(function (campaign) {
-      console.log('- ' + campaign.product.name);
-    })
+  response.obj.data.forEach(function (campaign) {
+    console.log('- ' + campaign.product.name);
   })
-  .catch(function (error) {
-    console.error(error);
-  });
+})
+.catch(function (error) {
+  console.error(error);
+});
